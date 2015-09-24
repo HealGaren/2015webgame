eec09abc89914e961876bcbd4e740a91bf8afb6f
HealGaren <qkswkd7003@naver.com>
2015-09-24 20:35:20 +0900

    객체 분리 중

---
+++ player.js
@@ -0,0 +1,10 @@
+﻿function Player(){
+	this.pos = new Pos(500, 500);
+	this.targetPos = new Pos(500, 500);
+	this.speed = 10;
+}
+
+Player.prototype.update = function(){
+	playerPosX += (targetX - playerPosX)/20;
+	playerPosY += (targetY - playerPosY)/20;
+}
---
+++ pos.js
@@ -0,0 +1,4 @@
+﻿function Pos(x, y){
+	this.x = x;
+	this.y = y;
+}
